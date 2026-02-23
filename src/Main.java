6405194041a892ff6ff6642e36ca6e08ae5c9e06
Cscience1 <dipinamamogobo@gmail.com>
2026-02-23 15:12:52 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,18 @@ public class Main {
     }
 
     private static int binarysearch(int[] X, int target) {
+        int low = 0;
+        int high = X.length - 1;
 
+        while(low<=high){
+            int mid = (low+high)/2;
+
+            if(X[mid]==target) return mid;
+            else if(X[mid]<target){
+                high = mid - 1;
+            }else low = mid + 1;
+        }
+        return -1;
     }
 
     private static int linearsearch(int[] X, int target) {
