e7be80af81e7e8f5a21d43693eac135afd575636
Cscience1 <dipinamamogobo@gmail.com>
2026-02-23 15:23:27 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,54 @@
 
+public class timeMethods{
+    public static int N = 32654;
+    public static void main(String[] args){
 
-public class Main {
-    public static void main(String[] args) {
-        int[] X = new int[32654];
+        DecimalFormat twoD = new DecimalFormat("0.00");
+        DecimalFormat fourD = new DecimalFormat("0.0000");
+        DecimalFormat fiveD = new DecimalFormat("0.00000");
 
-        int lin = linearsearch(X,70);
-        int ban = binarysearch(X,45);
+        long start, finish;
+        double runTime = 0, runTime2 = 0, time;
+        double totalTime = 0.0;
+        int n = N;
+        int repetition, repetitions = 30;
 
-        System.out.println(lin);
+        runTime = 0;
+        for(repetition = 0; repetition < repetitions; repetition++) {
+            start = System.currentTimeMillis();
 
-    }
+            // call the procedures to time here:
+            linearsearch(...);
+            binarysearch (...);
+            // Figure out how to alter this guideline here,
 
-    private static int binarysearch(int[] X, int target) {
+            finish = System.currentTimeMillis();
+
+            time = (double)(finish - start);
+            runTime += time;
+            runTime2 += (time*time); }
+
+        double aveRuntime = runTime/repetitions;
+        double stdDeviation =
+                Math.sqrt(runTime2 - repetitions*aveRuntime*aveRuntime)/(repetitions-1);
+
+        System.out.printf("\n\n\fStatistics\n");
+        System.out.println("________________________________________________");
+        System.out.println("Total time   =           " + runTime/1000 + "s.");
+        System.out.println("Total time\u00b2  =           " + runTime2);
+        System.out.println("Average time =           " + fiveD.format(aveRuntime/1000)
+                + "s. " + '\u00B1' + " " + fourD.format(stdDeviation) + "ms.");
+        System.out.println("Standard deviation =     " + fourD.format(stdDeviation));
+        System.out.println("n            =           " + n);
+        System.out.println("Average time / run =     " + fiveD.format(aveRuntime/n*1000)
+                + '\u00B5' + "s. ");
+
+        System.out.println("Repetitions  =             " + repetitions);
+        System.out.println("________________________________________________");
+        System.out.println();
+        System.out.println(); }
+
+    public static int binarysearch(int[] X, int target) {
         int low = 0;
         int high = X.length - 1;
 
@@ -26,7 +63,7 @@ public class Main {
         return -1;
     }
 
-    private static int linearsearch(int[] X, int target) {
+    public static int linearsearch(int[] X, int target) {
 
         for(int i = 0; i < X.length; i++){
             if(X[i]==target){
